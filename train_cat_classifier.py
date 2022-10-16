069a2f087fbe5aebf4ade8ce5d46ad17e6182a8f
MarinaWyss <marina.bennett.wyss@gmail.com>
2022-10-16 15:45:47 -0700

    fix copy pasta

--- train_cat_classifier.py
+++ train_cat_classifier.py
@@ -5,10 +5,10 @@ with open('config.yaml', 'r') as file:
     configs = yaml.safe_load(file)
 
 def main(args):
-    dnn_skill_tagging = CatClassifier(args)
-    dnn_skill_tagging.construct()
-    dnn_skill_tagging.fit()
-    dnn_skill_tagging.save()
+    cat_classifier = CatClassifier(args)
+    cat_classifier.construct()
+    cat_classifier.fit()
+    cat_classifier.save()
 
 
 if __name__ == '__main__':
