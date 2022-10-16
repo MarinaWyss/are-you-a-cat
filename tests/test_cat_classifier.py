0e851552ba9cfa38d11ab8295d48fe0d0c275150
MarinaWyss <marina.bennett.wyss@gmail.com>
2022-10-16 15:06:05 -0700

    add tests

---
+++ tests/test_cat_classifier.py
@@ -0,0 +1,71 @@
+import yaml
+
+import unittest
+import unittest.mock as mock
+
+from cat_classifier import CatClassifier
+
+
+class TestCatClassifier(unittest.TestCase):
+    """Test the CatClassifier model."""
+    @classmethod
+    def setUpClass(cls):
+        with open('config_test.yaml', 'r') as file:
+            configs = yaml.safe_load(file)
+
+        cls.args = configs['training']
+
+    def test_initialize_class(self):
+        # Initialize class
+        cnn2d = CatClassifier(self.args)
+
+        # Assert args are correct
+        self.assertEqual(cnn2d.args.kernel_size, 3)
+        self.assertEqual(cnn2d.args.val_split, 0.2)
+        self.assertEqual(cnn2d.args.output_path, 'model_test.h5')
+
+        # Assert model is None
+        self.assertIsNone(cnn2d.model)
+
+    def test_construct(self):
+        # Construct model
+        cnn2d = CatClassifier(self.args)
+        cnn2d.construct()
+        model = cnn2d.model
+
+        # Assert model is constructed correctly
+        self.assertIsNotNone(model)
+        self.assertEqual(21, len(model.layers))
+        self.assertEqual(2, len(model.outputs))
+
+    def test_fit(self):
+        cnn2d = CatClassifier(self.args)
+
+        with self.assertRaises(Exception) as ex:
+            cnn2d.fit()  # Fit model that hasn't been initialized yet
+        # Assert failure message is correct
+        self.assertEqual('Error: Initialize model before fitting.',
+                         str(ex.exception))
+
+    def test_save_model_not_initialized(self):
+        cnn2d = CatClassifier(self.args)
+
+        with self.assertRaises(Exception) as ex:
+            cnn2d.save()  # Save model that hasn't been initialized yet
+        # Assert failure message is correct
+        self.assertEqual('Error: Initialize model before saving.',
+                         str(ex.exception))
+
+    @mock.patch('tensorflow.keras.models.save_model')
+    def test_save(self, save_model):
+        cnn2d = CatClassifier(self.args)
+        cnn2d.construct()
+        cnn2d.save()
+
+        save_model.assert_called_once_with(cnn2d.model,
+                                           cnn2d.args.output_path,
+                                           save_format='h5')
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ tests/test_train_cat_classifier.py
@@ -0,0 +1,35 @@
+import yaml
+
+import unittest
+import unittest.mock as mock
+
+from cat_classifier import CatClassifier
+
+
+class TestDNNSkillTagging(unittest.TestCase):
+    """Test DNN skill tagging."""
+    @classmethod
+    def setUpClass(cls):
+        with open('config_test.yaml', 'r') as file:
+            configs = yaml.safe_load(file)
+
+        cls.args = configs['training']
+
+    @mock.patch('cat_classifier.CatClassifier.construct')
+    @mock.patch('cat_classifier.CatClassifier.fit')
+    @mock.patch('cat_classifier.CatClassifier.save')
+    def test_main(self,
+                  cat_classifier_construct,
+                  cat_classifier_fit,
+                  cat_classifier_save):
+
+        CatClassifier.main(self.args)
+
+        # Assert model fitted and saved
+        cat_classifier_construct.assert_called_once()
+        cat_classifier_fit.assert_called_once()
+        cat_classifier_save.assert_called_once()
+
+
+if __name__ == '__main__':
+    unittest.main()
