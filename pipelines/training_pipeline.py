b130f90f1a5b2433cdb668860c27b68b349b9752
MarinaWyss <marina.bennett.wyss@gmail.com>
2022-10-22 11:15:21 -0700

    placeholder

--- pipelines/training_pipeline.py
+++ pipelines/training_pipeline.py
@@ -10,10 +10,10 @@ def train_pipeline(import_data, train_model, evaluate_model):
         evaluate_model: DataClass
 
     Returns:
-        (placeholder) Output from eval
+        (float, float, float) precision, recall, and f1 scores
+            from test set evaluation
     """
     X_train, y_train, X_test, y_test = import_data()
     model = train_model(X_train, y_train)
-    # TODO actually finish this
-    placeholder = evaluate_model(model, X_test, y_test)
-    return placeholder
+    precision, recall, f1 = evaluate_model(model, X_test, y_test)
+    return precision, recall, f1
